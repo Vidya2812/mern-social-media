089b1927a222e22ae6da2abdbab7ece678be77ca
Sameekshaa18 <20z338@psgtech.ac.in>
2023-04-05 13:54:21 +0530

    initial commit - full project

---
+++ client/src/components/header/Header.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Menu from "./Menu";
+import Search from "./Search";
+import { useDispatch, useSelector } from "react-redux";
+import { getPosts } from '../../redux/actions/postAction';
+import { getSuggestions } from '../../redux/actions/suggestionsAction';
+import logo1 from "./logoConnect.png";
+
+const Header = () => {
+  const { auth } = useSelector(state => state);
+  const dispatch = useDispatch();
+
+  const handleRefreshHome = () => {
+    window.scrollTo({top: 0})
+    dispatch(getPosts(auth.token));
+    dispatch(getSuggestions(auth.token));
+  };
+
+  return (
+    <div className="header bg-light">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
+        <div className="container-fluid">
+          <Link to="/" className="logo" onClick={handleRefreshHome}>
+            <h1 className="navbar-brand  p-0 m-0"><img style={{width:"20%",height:"30%"}} src={logo1}/ >
+Connect</h1>
+          </Link>
+
+          <Search />
+
+          <Menu />
+         
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
